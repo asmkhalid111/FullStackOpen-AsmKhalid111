6709b14a64eaf6fbf6a21e0896e4dcd9c042cf73
AsmKhalid <130174468+asmkhalid111@users.noreply.github.com>
2025-01-03 21:54:19 +0600

    Updated

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,28 @@
+//! Question =>  Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props. Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the total number of exercises.
+
+// Define the new components in the file App.jsx.
+
+//* The App component's body will approximately be as follows:
+
+//* const App = () => {
+//* const-definitions
+
+//*   return (
+//*     <div>
+//*       <Header course={course} />
+//*       <Content ... />
+//*       <Total ... />
+//*     </div>
+//*   )
+//* }
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+
+const Content = (props)
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,7 +34,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
+      <Header course={course} />
       <p>
         {part1} {exercises1}
       </p>
